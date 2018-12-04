3cd77ddfbdc28506f8d2c39fb427dbdc11e42467
Natsu Merlin <jamjamerlin@gmail.com>
2018-12-04 09:42:02 +0800

    Version 0.1: Crashing GUI

--- views/gui.py
+++ views/gui.py
@@ -7,6 +7,12 @@ class GUIInterface(Interface):
 	def __init__(self):
 		super().__init__()
 
+	def on_won(self):
+		self.gui.on_won()
+
+	def on_lost(self):
+		self.gui.on_lost()
+
 	def run(self):
 		self.initialize_event_handlers()
 		self.view_events.create(size = 6, initial_value = 3,
